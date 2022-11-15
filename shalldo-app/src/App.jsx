6b8da18a10eee9270ebeae24b1188965c01b76d5
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-15 15:12:47 +0100

    f

--- shalldo-app/src/App.jsx
+++ shalldo-app/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import "./App.css";
 
 function App() {
-    return (<div className="App">
-      <HomePage />
-    </div>);
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- shalldo-app/src/components/HomePage.js
+++ shalldo-app/src/components/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function HomePage() {
+function HomePage() {
     return (
         <>
-            <h1>HomePage</h1>
+            <h1>Home</h1>
         </>
     );
 }
+export default HomePage;
